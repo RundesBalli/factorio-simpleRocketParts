3dd469bc095c8c89bcc1f61be9fbb0eeb2a1d724
RundesBalli <GitHub@RundesBalli.com>
2023-10-02 13:07:12 +0200

    Balancing

--- SimpleRocketParts/recipe.lua
+++ SimpleRocketParts/recipe.lua
@@ -1,4 +1,4 @@
 data.raw["recipe"]["rocket-part"].ingredients = {{"rocket-control-unit", 1},{"rocket-fuel", 1}}
 data.raw["recipe"]["rocket-control-unit"].ingredients = {{"iron-plate", 45},{"copper-plate", 45}}
 data.raw["recipe"]["rocket-fuel"].ingredients = {{"coal", 90}}
-data.raw["recipe"]["satellite"].ingredients = {{"iron-plate", 9000},{"copper-plate", 4500}}
+data.raw["recipe"]["satellite"].ingredients = {{"iron-plate", 1800},{"copper-plate", 900}}

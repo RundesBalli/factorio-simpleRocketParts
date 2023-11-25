a5a5d6278a2eda3d3e10031f869408a616ef30c7
RundesBalli <GitHub@RundesBalli.com>
2023-11-25 22:21:00 +0100

    Added space science pack dump

--- SimpleRocketParts/recipe.lua
+++ SimpleRocketParts/recipe.lua
@@ -2,3 +2,4 @@ data.raw["recipe"]["rocket-part"].ingredients = {{"rocket-control-unit", 1},{"ro
 data.raw["recipe"]["rocket-control-unit"].ingredients = {{"iron-plate", 45},{"copper-plate", 45}}
 data.raw["recipe"]["rocket-fuel"].ingredients = {{"coal", 90}}
 data.raw["recipe"]["satellite"].ingredients = {{"iron-plate", 1800},{"copper-plate", 900}}
+data.raw["recipe"]["heavy-armor"].ingredients = {{"space-science-pack", 50000}}
